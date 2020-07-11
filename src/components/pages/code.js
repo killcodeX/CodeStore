3c0d3b002290d38cc4130450633cec95cfc49c84
Aaquib Ahmed <aaquibskimbox@gmail.com>
2020-07-11 19:55:18 +0530

    css fixed

--- src/components/pages/code.js
+++ src/components/pages/code.js
@@ -8,8 +8,10 @@ export default function Code(props) {
         <>
           <div className='box'>
             <h2 className='is-size-4'>{desc}</h2>
-            <div className='has-text-right'>Type: {type}</div>
-            <div className='button btn'>Know More</div>
+            <div className='detail'>
+              <div className='button btn'>Know More</div>
+              <div className='is-size-6 has-text-weight-medium'>Type: {type}</div>
+            </div>
           </div>
         </>
     )
