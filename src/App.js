a076714aa9a1dd601dbe8bdc8c968f21e568eac1
Aaquib Ahmed <aaquibskimbox@gmail.com>
2020-07-12 16:10:03 +0530

    login form done

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import Home from './components/pages/home';
 import { StateManager } from './components/context/StateManager';
 import AddCode from './components/pages/addCode';
-import CodeDetail from './components/pages/codeDetail';  
+import CodeDetail from './components/pages/codeDetail';
+import Login from './components/pages/login';  
 import {
   BrowserRouter as Router,
   Switch,
@@ -20,6 +21,7 @@ function App() {
           <Route exact path='/' component={Home}/>
           <Route exact path='/addcode' component={AddCode}/>
           <Route exact path='/code/:id' component={CodeDetail}/>
+          <Route exact path='/login' component={Login}/>
         </Switch>
       </BrowserRouter>
     </StateManager>
---
+++ src/components/pages/login.js
@@ -0,0 +1,57 @@
+import React,{ useState } from 'react';
+import BG from '../images/bg.svg';
+import Avatar from '../images/avatar.svg';
+
+export default function Login() {
+
+    const [name, setName ] = useState('');
+    const [password, setPassword ] = useState('');
+
+    const submit = e => {
+        e.preventDefault();
+        console.log('name',name);
+        console.log('password',password)
+    }
+
+
+    return (
+        <>
+            <div className='section' id='login'>
+                <div className='container'>
+                    <div className='columns'>
+                        <div className='column'>
+                            <img className='vector-bg' src={BG} alt='vector-login'/>
+                        </div>
+                        <div className='column'>
+                            <img className='avatar-bg' src={Avatar} alt='avatar-login'/>
+                            <div className='login-form'>
+                                <h2 className=' is-size-3 has-text-centered'>WELCOME</h2>
+                                <form onSubmit={submit}>
+                                    <div className='field'>
+                                        <label className="label">Email</label>
+                                        <div className="control has-icons-left has-icons-right">
+                                            <input className="input" type="text" placeholder="Enter your Email.." value={name} onChange={ e => setName(e.target.value)}/>
+                                            <span className="icon is-small is-left">
+                                                <i class="fas fa-envelope"></i>
+                                            </span>  
+                                        </div>
+                                    </div>
+                                    <div className='field'>
+                                        <label className="label">Password</label>
+                                        <div className="control has-icons-left has-icons-right">
+                                            <input className="input" type="password" placeholder="Enter your password.." value={password} onChange={ e => setPassword(e.target.value)}/>
+                                            <span className="icon is-small is-left">
+                                                <i class="fas fa-lock"></i>
+                                            </span>  
+                                        </div>
+                                    </div>
+                                    <button className='button' type='submit'>Login</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
