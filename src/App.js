624dc431274831624eadc8c087140c22f56f030a
Aaquib Ahmed <aaquibskimbox@gmail.com>
2020-07-12 17:00:43 +0530

    added 404 error page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Home from './components/pages/home';
 import { StateManager } from './components/context/StateManager';
 import AddCode from './components/pages/addCode';
 import CodeDetail from './components/pages/codeDetail';
-import Login from './components/pages/login';  
+import Login from './components/pages/login';
+import Errorpage from './components/pages/errorpage';  
 import {
   BrowserRouter as Router,
   Switch,
@@ -22,6 +23,7 @@ function App() {
           <Route exact path='/addcode' component={AddCode}/>
           <Route exact path='/code/:id' component={CodeDetail}/>
           <Route exact path='/login' component={Login}/>
+          <Route component={Errorpage}/>
         </Switch>
       </BrowserRouter>
     </StateManager>
---
+++ src/components/pages/errorpage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Errorimg from '../images/404.svg';
+
+export default function Errorpage() {
+
+    const mystyle = {
+        width: "700px",
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%,50%)'
+      };
+
+    return (
+        <div>
+            <div className='container'>
+                <img style={mystyle}src={Errorimg} alt='error page' />
+            </div>
+        </div>
+    )
+}
